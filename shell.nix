cd40a8769b22c562841f7c9049ab1b1cf111b866
Eric Merritt <eric@merritt.tech>
2016-01-14 11:14:15 -0800

    version bump 0.0.2

--- shell.nix
+++ shell.nix
@@ -7,7 +7,7 @@ let
   f = { buildRebar3, ibrowse, jsx, erlware_commons, getopt }:
       buildRebar3 {
         name = "hex2nix";
-        version = "0.0.1";
+        version = "0.0.2";
         src = ./.;
         erlangDeps = [ ibrowse jsx erlware_commons getopt ];
       };
